21eda67453d12cc602cc9cb84f24cadd63c9f426
TheLariska <121815263+TheLariska@users.noreply.github.com>
2023-02-03 14:14:38 +0300

    Update Meetly hub.lua

--- Meetly hub.lua
+++ Meetly hub.lua
@@ -3534,11 +3534,9 @@ local Section = Tab:NewSection("BETA TEST: Minigun#8496")
 
 local Tab = Window:NewTab("Update")
 
-local Section = Tab:NewSection("changelog 2.1")
+local Section = Tab:NewSection("changelog 2.2")
 
-local Section = Tab:NewSection("[/] fix noclip")
-
-local Section = Tab:NewSection("[+] New Speed bypass")
+local Section = Tab:NewSection("[+] Anti-Kick")
 
 local Tab = Window:NewTab("settings")
 
@@ -3546,6 +3544,41 @@ local Section = Tab:NewSection("settings")
 
 Section:NewKeybind("open/close hub", "Close/Open UI", Enum.KeyCode.BackSlash, function()
     Library:ToggleUI()
+end)
+
+Section:NewButton("Anti-kick","-", function()
+wait()
+                local hook = game:GetService("Players").LocalPlayer.Kick;
+                local mt = getrawmetatable(game:GetService("Players").LocalPlayer)
+                local psuedoEnv = {
+                    ["__index"] = mt.__index,
+                    ["__namecall"] = mt.__namecall
+                }
+                local ReturnFunc = function()end
+                        
+                warn("Made By TheMeetly#0885")
+                setreadonly(mt, false)
+                mt.__namecall = newcclosure(function(self, Index, ...)
+                    local NameCallMethod = getnamecallmethod()
+                    if type(NameCallMethod) == "string" then
+                        if NameCallMethod == "Kick" or NameCallMethod == "kick" then
+                            print("Prevented Kick At " .. tostring(os.time()))
+                            return ReturnFunc
+                        end
+                    end
+                    return psuedoEnv.__namecall(self, Index, ...)
+                end)
+                        
+                mt.__index = newcclosure(function(self, Index, ...)
+                    if type(Index) == "string" then
+                        if Index == "Kick" or Index == "kick" then
+                            print("Prevented Kick At " .. tostring(os.time()))
+                            return ReturnFunc
+                        end
+                    end
+                    return psuedoEnv.__index(self, Index, ...)
+                end)
+                setreadonly(mt, true)
 end)
 
 Section:NewButton("low lag", "-", function()
